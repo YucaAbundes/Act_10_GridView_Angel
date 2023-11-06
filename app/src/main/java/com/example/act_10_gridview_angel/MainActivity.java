c29162e11d5de17abf9f3e231dd4122a6f3e2d87
YucaAbundes <aaarteaga.abundes@gmail.com>
2023-11-06 06:24:55 -0600

    vfinal

---
+++ app/src/main/java/com/example/act_10_gridview_angel/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.act_10_gridview_angel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] nombresFiguras = {"HEXAGONO", "TRIANGULO ESCALENO", "CILINDRO", "CIRCULO"};
+
+    int[] listaImagenes = {R.drawable.hexagono, R.drawable.triangulo, R.drawable.cilindro, R.drawable.circulo};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Cambiamos a un GridView
+        final GridView gridView = findViewById(R.id.gridView);
+
+        BaseAdapterPersonalizado adapterPersonalizado = new BaseAdapterPersonalizado(getApplicationContext(), nombresFiguras, listaImagenes);
+
+        gridView.setAdapter(adapterPersonalizado);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String figuraSeleccionada = nombresFiguras[position];
+
+                Intent intent = null;
+
+                try {
+                    switch (figuraSeleccionada) {
+                        case "HEXAGONO":
+                            intent = new Intent(MainActivity.this, DetalleHexagonoActivity.class);
+                            break;
+                        case "TRIANGULO ESCALENO":
+                            intent = new Intent(MainActivity.this, DetalleTrianguloActivity.class);
+                            break;
+                        case "CILINDRO":
+                            intent = new Intent(MainActivity.this, DetalleCilindroActivity.class);
+                            break;
+                        case "CIRCULO":
+                            intent = new Intent(MainActivity.this, DetalleCirculoActivity.class);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (intent != null) {
+                        intent.putExtra("figuraSeleccionada", figuraSeleccionada);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "No se encontró la actividad de detalle para " + figuraSeleccionada, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
